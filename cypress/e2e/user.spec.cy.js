bab770e8305e11bc020e11cd0bc86093097a1464
vitoria nascimento <vivanascimento2020@gmail.com>
2025-02-15 19:06:41 -0300

    Page Object Login

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,41 +1,44 @@
 import userData from '../fixtures/userData.json'
+import LoginPage from '../pages/loginPage'
+
+const login=new LoginPage()
+
 describe('Orange HRM Tests', () => {
 
   const selectorsList={
-    usernameField: '[name="username"]',
-    passwordField: '[name="password"]',
-    loginButton:    '[type="submit"]',
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     dashboardGrid: ".oxd-topbar-header-breadcrumb-module",
-    wrongCredentialAlert: '[role="alert"]',
     myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
     firstNameField:'[name="firstName"]',
     lastNameField:'[name="lastName"]',
     genericField: ".oxd-input--active",
     licenseDate: '[placeholder="yyyy-dd-mm"]',
     bottomClose: '.--close',
-    botomSaveMyinfo:  '[type="submit"]'
-  
+    botomSaveMyinfo:  '[type="submit"]',
+    dropdownField: ".oxd-select-wrapper"
 
   }
 
   
   it.only('User Info Update Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSucess.userName)
-    cy.get(selectorsList.passwordField).type(userData.userSucess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
-    cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type("First Name Teste")
-    cy.get(selectorsList.lastNameField).clear().type("Last Name Teste")
-    cy.get(selectorsList.genericField).eq(4).clear().type("Employeed")
-    cy.get(selectorsList.genericField).eq(5).clear().type("Driver")
-    cy.get(selectorsList.genericField).eq(6).clear().type("2025-04-01")
-    cy.get(selectorsList.bottomClose).click()
-    cy.get(selectorsList.botomSaveMyinfo).eq(0).click()
-    cy.get('body').should('contain','Successfully Updated')
 
+    login.acessLoginPage()
+    login.loginWithUser(userData.userSucess.userName,userData.userSucess.password)
+
+    //cy.location('pathname').should('equal','/web/index.php/dashboard/index')
+    //cy.get(selectorsList.myInfoButton).click()
+    //cy.get(selectorsList.firstNameField).clear().type("First Name Teste")
+    //cy.get(selectorsList.lastNameField).clear().type("Last Name Teste")
+    //cy.get(selectorsList.genericField).eq(4).clear().type("Employeed")
+    //cy.get(selectorsList.genericField).eq(5).clear().type("Driver")
+    //cy.get(selectorsList.genericField).eq(6).clear().type("2025-04-01")
+    //cy.get(selectorsList.bottomClose).click()
+    //cy.get(selectorsList.dropdownField).eq(0).click()
+    //cy.get(':nth-child(4) > span').click()
+    //cy.get(selectorsList.dropdownField).eq(1).click({force:true})
+    //cy.get(':nth-child(2) > span').click({force:true})
+    //cy.get(selectorsList.botomSaveMyinfo).eq(0).click()
+    //cy.get('body').should('contain','Successfully Updated')
     //cy.get(selectorsList.genericField).eq(4).type("Nick")//aqui uso uma combinaçao do nome da classe e a posicao do campo para elementos que nao possuem atributos unicos
 
   })
