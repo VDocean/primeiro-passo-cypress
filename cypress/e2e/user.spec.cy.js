499a66ca0f56996e65381544d246ddf0e7019b4a
vitoria nascimento <vivanascimento2020@gmail.com>
2025-02-16 18:55:36 -0300

    using ChanceJs for random data

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,10 +4,16 @@ import DashBoardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
 import MyInfoPage from '../pages/myInfoPage'
 
+const Chance=require('chance')
+
+const chance=new Chance()
 const login=new LoginPage()
 const dashPage=new DashBoardPage()
 const menuPage= new MenuPage()
 const myInfo=new MyInfoPage()
+
+
+
 
 describe('Orange HRM Tests', () => {
 
@@ -18,8 +24,8 @@ describe('Orange HRM Tests', () => {
     login.loginWithUser(userData.userSucess.userName,userData.userSucess.password)
     dashPage.verifyIfDashboard()
     menuPage.acessMyInfo()
-    myInfo.fillPersonalDetails("Chico","Chicao")
-    myInfo.fillEmployeeDetails('ImployeeId','driver','2025-02-14')
+    myInfo.fillPersonalDetails(chance.first(),chance.last())
+    myInfo.fillEmployeeDetails('ImployeeId','driver','2025-02-13')
     myInfo.fillStatus()
     myInfo.saveForm()
 
